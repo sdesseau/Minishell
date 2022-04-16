158151d6ace82d2b6e06c805d5816a1a9393e785
Stanislas DESSEAUX <sdesseau@e1r10p9.42.fr>
2022-04-16 14:51:44 +0200

    push stan

---
+++ minishell-new/source/parse/utils_pars.c
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils_pars.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sdesseau <sdesseau@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/15 14:08:12 by sdesseau          #+#    #+#             */
+/*   Updated: 2022/04/16 13:49:57 by sdesseau         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../inc/minishell.h"
+
+t_cmd	init_cmd(t_cmd data)
+{
+	data.nb_chevrons = 0;
+	data.pipe = 0;
+	return (data);
+}
+
+t_pars	*put_lock(t_pars *pars)
+{
+	int	i;
+
+	i = 0;
+	while (pars[i].i != -1)
+	{
+		if (pars[i].s_q == 1 || pars[i].d_q == 1)
+			pars[i].lock = 1;
+		else
+			pars[i].lock = 0;
+		i++;
+	}
+	return (pars);
+}
+
+t_cmd	*empty_line(t_cmd *data)
+{
+	printf("exit\n");
+	exit(131);
+	return (data);
+}
+
+int	pass_spaces(t_pars *pars, int i)
+{
+	while (pars[i].val == ' ' && pars[i].s_q == 0
+		&& pars[i].d_q == 0 && pars[i].i != -1)
+	{
+		i++;
+		if (pars[i].null == 1)
+			break ;
+	}
+	return (i);
+}
+
+int	is_first_pipe(t_pars *pars)
+{
+	int		i;
+
+	i = 0;
+	i = pass_spaces(pars, i);
+	if (pars[i].val == '|' && pars[i].lock == 0)
+	{
+		syntax_error('|');
+		return (-1);
+	}
+	return (0);
+}
