bfdbae790ffdae15abb7d0dbf0df7a1364ea2c0d
Manon Prigent <manonprigent@macbook-pro-de-manon.home>
2022-04-21 23:32:12 +0200

    error messages

--- minishell-new/source/exec/external_cmd.c
+++ minishell-new/source/exec/external_cmd.c
@@ -6,7 +6,7 @@
 /*   By: mprigent <mprigent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/18 16:19:27 by mprigent          #+#    #+#             */
-/*   Updated: 2022/04/21 22:34:08 by mprigent         ###   ########.fr       */
+/*   Updated: 2022/04/21 23:31:02 by mprigent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,43 +57,36 @@ int	ft_check_permission(char **cmd, char *ext_cmd,
 	return (1);
 }
 
-// int	check(char **cmd, t_env *env)
-// {
-// 	struct stat	f;
-// 	int			flag;
-
-// 	if (lstat(cmd[0], &f) != -1)
-// 	{
-// 		flag = f.st_mode & S_IFMT;
-// 		if (flag == S_IFDIR && (cmd[0][ft_strlen(cmd[0]) - 1] == '/' || ft_strncmp(cmd[0], "./", 2)))
-// 			return (ft_error(cmd[0], ": Is a directory\n", 0, 126));
-// 		else if (flag != S_IXUSR && flag != S_IRUSR && flag != S_IFDIR && flag != S_IFLNK)
-// 			return (ft_error(cmd[0], ": Permission denied\n", 0, 126));
-// 		else if ((ft_strncmp(cmd[0], "./", 2) || ft_strncmp(cmd[0], "/", 1)) && (f.st_mode & S_IXUSR) && (f.st_mode & S_IRUSR))
-// 			return (ft_check_permission(cmd, ft_strdup(cmd[0]), f, env));
-// 	}
-// 	else if (ft_strncmp(cmd[0], "./", 2) || cmd[0][0] == '/')
-// 	{
-// 		return (ft_error(cmd[0], ": No such file or directory\n", 0, 127));
-// 	}
-// 	return (ft_error(cmd[0], ": command not found\n", 0, 127));
-// }
-
 int	ft_execute_external_cmd(char **cmd, t_env *env)
 {
 	struct stat	statbuf;
 	char		**path;
 	char		*ext_cmd;
+	int			flag;
 	size_t		i;
 
 	path = ft_get_path(&env);
 	if (!path)
-		return (-1);
+	{
+			printf("minishell: %s: No such file or directory\n", cmd[0]);
+			exit(127);
+	}
 	if (lstat(cmd[0], &statbuf) != -1)
 	{
+		flag = statbuf.st_mode & S_IFMT;
 		if ((ft_strncmp(cmd[0], "./", 2) || ft_strncmp(cmd[0], "/", 1)) && (statbuf.st_mode & S_IXUSR) && (statbuf.st_mode & S_IRUSR))
 		{
 			return (ft_check_permission(cmd, ft_strdup(cmd[0]), statbuf, env));
+		}
+		else if ((flag == S_IFDIR && (cmd[0][ft_strlen(cmd[0]) - 1] == '/' || ft_strncmp(cmd[0], "./", 2))))
+		{
+			printf("minishell: %s: Is a directory\n", cmd[0]);
+			exit(126);	
+		}
+		else if (flag != S_IXUSR && flag != S_IRUSR && flag != S_IFDIR && flag != S_IFLNK)
+		{
+			printf("minishell: %s: Permission denied\n", cmd[0]);
+			exit(126);
 		}
 	}
 	i = 0;
@@ -108,7 +101,7 @@ int	ft_execute_external_cmd(char **cmd, t_env *env)
 		i++;
 		if ((cmd[0][0] == '.' && cmd[0][1] == 0) || (cmd[0][0] == '.' && cmd[0][1] == '.' && cmd[0][2] == 0) || !path[i])
 		{
-			printf("minishell: %s: command not found\n", cmd[0]);
+			printf("minishell: %s: Command not found\n", cmd[0]);
 			exit(127);
 		}
 	}
